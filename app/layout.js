1a52f5d62d18bb0a6d7874d1ddb660b9c78e0d4f
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-05-30 07:16:02 +0200

    making Header, Logo and Navigation components

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -24,11 +25,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
+        <Header />
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
