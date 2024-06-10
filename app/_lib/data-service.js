d91dbab2a8744f0c312b735fdc0e31a3a436fd31
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-06-10 13:24:11 +0200

    updating user profile with server actions

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -165,6 +165,7 @@ export async function createGuest(newGuest) {
   return data;
 }
 
+/*
 export async function createBooking(newBooking) {
   const { data, error } = await supabase
     .from("bookings")
@@ -227,3 +228,4 @@ export async function deleteBooking(id) {
   }
   return data;
 }
+*/
