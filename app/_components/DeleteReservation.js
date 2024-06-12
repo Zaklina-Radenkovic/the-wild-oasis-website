df5cd48572e274229ed2bf378e079ce34e7dc48a
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-06-12 18:11:22 +0200

    implementing useOptimistic hook for deleted reservations

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,15 +1,14 @@
 "use client";
-import { useTransition } from "react";
 import { TrashIcon } from "@heroicons/react/24/solid";
+import { useTransition } from "react";
 import SpinnerMini from "./SpinnerMini";
-import { deleteReservation } from "../_lib/actions";
 
-function DeleteReservation({ bookingId }) {
+function DeleteReservation({ bookingId, onDelete }) {
   const [isPending, startTransition] = useTransition();
 
   function handleDelete() {
     if (confirm("Are you sure you want to delete this reservation?"))
-      startTransition(() => deleteReservation(bookingId));
+      startTransition(() => onDelete(bookingId));
   }
   return (
     <button
